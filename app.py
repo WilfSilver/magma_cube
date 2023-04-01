65a0e7d3f9e27f8e04372f46ed475f198b919fb8
Sammy99jsp <sammy99jsp@gmail.com>
2023-04-01 14:19:34 +0100

    Linting + `Pillow`

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
 import traceback as tb
 
 import colorama
-
-import numpy as np
-
-import moderngl
+import typing
 import moderngl_window as mglw
 
-## version_string
+if typing.TYPE_CHECKING:
+
 def gl_version(version_string="4.4"):
     version = version_string.split(".")
     return tuple(int(i) for i in version)
 
 
-def panic(msg : str):
+def panic(msg: str) -> None:
     """
     panic! from Rust.
     """
-    print(colorama.Fore.RED + f"PANIC! {msg}\nStack Trace:" + colorama.Fore.RESET)
-    list(map(print, map(lambda s: f"\t{s}", tb.format_tb())))
+    print(
+          f"{colorama.Fore.RED}PANIC! {msg}\nStack Trace:{colorama.Fore.RESET}"
+         )
+    list(map(print, map(lambda s: f"\t{s}", tb.format_tb(None))))
     exit(7)
+
 
 def get_shader(filename: str) -> str:
     with open(filename, 'r', encoding="utf8") as shader_file:
@@ -40,13 +41,11 @@ class MagmaWindow(mglw.WindowConfig):
             get_shader("compute.glsl")
         )
 
-
     def render(self, time, frametime):
         print("CALLED!")
         # ctx = self.ctx
         # self.ctx.clear(1.0, 1.0, 0.0, 0.0)
-       
-    
+
 
 def window():
     MagmaWindow.run()
