7f0fa859e912a70c1016d321c45c802cbb055b7e
Sammy99jsp <sammy99jsp@gmail.com>
2023-04-01 19:39:47 +0100

    Add picture processing.
    Co-authored-by: Mr-Wade3 <Mr-Wade3@users.noreply.github.com>

--- process_image.py
+++ process_image.py
@@ -1,9 +1,40 @@
-from typing import List, Tuple
+from typing import Any, Callable, Dict, Tuple, cast
 from PIL import Image, ImageOps
+from functools import reduce
 import numpy as np
 
 
-def food_points(filename: str, FOOD_POINT_RATIO=0.1) -> List[Tuple[int, int]]:
+def set_array(arr: np.ndarray, value: int) -> Callable[[Any], None]:
+    def __inner(index: Any) -> None:
+        arr[index] = value
+
+    return __inner
+
+def analyze_colors(image: Image.Image, TOTAL_PIXELS : int, THRESHOLDS: Tuple[float, float, float]):
+    def __inner(i: int) -> Tuple[Image.Image, float, int]:
+        clamped = image.convert(  # type:ignore
+            "P", colors=i, palette=Image.Palette.ADAPTIVE
+        )
+
+        colors = list(
+            cast(Dict[Tuple[int, int, int], int], clamped.palette.colors).keys()
+        )
+
+        brightness = max(
+            map(
+                lambda c: (c[0], colors[c[1]], c[1]),
+                clamped.getcolors(i)
+            ),
+            # https://alienryderflex.com/hsp.html
+            key=lambda t: ((0.299 * t[1][0] ** 2 + 0.587 * t[1][1] ** 2 + 0.114 * t[1][2] ** 2) ** 0.5)
+        )
+
+        return (clamped, abs(brightness[0] - THRESHOLDS[2]), brightness[2])
+
+    return __inner
+
+
+def food_points(filename: str, FOOD_POINT_RATIO=0.10, ACCEPTABLE_DELTA=0.05) -> Image.Image:
     """
     Gets food points from the bright spots of an image.
 
@@ -11,14 +42,26 @@ def food_points(filename: str, FOOD_POINT_RATIO=0.1) -> List[Tuple[int, int]]:
 
     :return List of (int, int) (x, y)-tuples
     """
-    FOOD_POINT_RATIO = 0.1
+
     with Image.open(filename) as image:
-        width, height = image.size
 
-        food_points = (int)(width * height * FOOD_POINT_RATIO)
+        TOTAL_PIXELS = reduce(lambda x, y: x * y, image.size)
+        COUNT_THRESHOLDS = (TOTAL_PIXELS * (FOOD_POINT_RATIO - ACCEPTABLE_DELTA), TOTAL_PIXELS * (FOOD_POINT_RATIO + ACCEPTABLE_DELTA), TOTAL_PIXELS * FOOD_POINT_RATIO)
 
-        image = ImageOps.grayscale(image)
-        data = np.array(image)
+        elem = min(map(analyze_colors(image, TOTAL_PIXELS=TOTAL_PIXELS, THRESHOLDS=COUNT_THRESHOLDS), range(2, 16)), key=lambda t: t[1]) # type: ignore
+        print(f"DEBUG: {elem[1] / TOTAL_PIXELS * 100}% off target ratio.")
 
-        sorted_data = sorted(np.ndenumerate(data), key=lambda t: -t[1])
-        return list(map(lambda t: t[0], sorted_data[:food_points]))
+        return clamp_image(image, elem[0], elem[2])
+
+
+def clamp_image(original_image: Image.Image, image: Image.Image, pallete_color: int) -> Image.Image:
+
+    for index, pixel in np.ndenumerate(image):
+        original_image.putpixel((index[1], index[0]), (255, 255, 255) if (pixel == pallete_color) else (0, 0, 0))
+
+    return original_image
+
+
+if __name__ == "__main__":
+    import sys
+    food_points(f"{sys.argv[1]}").save(f"clamped-{sys.argv[1]}")
